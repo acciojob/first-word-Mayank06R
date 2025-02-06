397ea7e109d4fb434fe3482939dccad9e9263af2
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-06 11:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	 let strippedString = str.trimStart();
+    let firstSpaceIndex = strippedString.indexOf(' ');
+    if (firstSpaceIndex === -1) {
+        return strippedString;
+    }
+    
+    // Return the substring from the start to the first space
+    return strippedString.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
